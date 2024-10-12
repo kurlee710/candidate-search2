adb31befa30b7df67d69c591b6aabe4dfcc66672
kurlee710 <josephatamaniuk@gmail.com>
2024-10-12 00:11:20 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   envDir: './env',
   plugins: [react()],
+  server: {
+    port: 5173, // Default port for development
+    host: '0.0.0.0', // Bind to all interfaces during development
+  },
+  preview: {
+    port: process.env.PORT || 4173, // Use Render's provided PORT in production or fallback
+    host: '0.0.0.0', // Bind to all interfaces for production preview mode
+  },
 });
